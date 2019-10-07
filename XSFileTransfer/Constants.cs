729196151f25e60e4f5d6d8ad72c615ccf023888
David Hofer <dave.hofer@hotmail.com>
2019-10-07 10:30:33 +0200

    adjusted maximum packet size

--- XSFileTransfer/Constants.cs
+++ XSFileTransfer/Constants.cs
@@ -2,6 +2,6 @@ namespace XSFileTransfer
 {
     public class Constants
     {
-        public const int MaxPacketSize = 0xFFFF;
+        public const int MaxPacketSize = 0x00FFFFFF;     // 512MByte seems to be the absolute maximum for this
     }
 }
