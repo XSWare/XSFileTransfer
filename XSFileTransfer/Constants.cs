7924a09ce07f7149a9c0208c38341d0687801758
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-07 20:17:31 +0200

    added some "sensible defaults"

--- XSFileTransfer/Constants.cs
+++ XSFileTransfer/Constants.cs
@@ -2,6 +2,9 @@ namespace XSFileTransfer
 {
     public class Constants
     {
-        public const int MaxPacketSize = 0x00FFFFFF;     // 512MByte seems to be the absolute maximum for this
+        public const int MaxPacketSize = 1 * 1024 * 1024;     // 512MByte seems to be the absolute maximum for this
+        public const int DefaultPort = 3648;
+
+        public const string DefaultReceiveFolder = "C:\\Receive";
     }
 }
