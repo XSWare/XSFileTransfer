84ad9834d4bdf980d94b7ab0d082c03c0a8090bc
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-14 21:40:47 +0200

    increased packet size to 32MBytes

--- XSFileTransfer/Constants.cs
+++ XSFileTransfer/Constants.cs
@@ -4,7 +4,7 @@ namespace XSFileTransfer
 {
     public class Constants
     {
-        public const int MaxPacketSize = 1 * 1024 * 1024;     // 512MByte seems to be the absolute maximum for this
+        public const int MaxPacketSize = 32 * 1024 * 1024;     // 512MByte seems to be the absolute maximum for this
         public const int DefaultPort = 3648;
         public const int DefaultTimeout = 10000;
 
