c623bc73128ac9ed19cf34b5745702c97ba0b3c9
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-15 19:29:16 +0200

    setting connection/thread count to 2 to avoid running into timeouts

--- XSFileTransfer/Constants.cs
+++ XSFileTransfer/Constants.cs
@@ -7,7 +7,7 @@ namespace XSFileTransfer
         public const int MaxPacketSize = 32 * 1024 * 1024;     // 512MByte seems to be the absolute maximum for this
         public const int DefaultPort = 3648;
         public const int DefaultTimeout = 10000;
-        public static int MaxConnections = 4;
+        public static int MaxConnections = 2;
 
         public static string DefaultReceiveFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\XSFileTransfer";
     }
