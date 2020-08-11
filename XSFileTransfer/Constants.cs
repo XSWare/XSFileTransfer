ac0b2420de4b179271a21ab0072990ac2b67e3b0
CaptainOachkatzl <dave.hofer@hotmail.com>
2020-08-11 22:11:47 +0200

    HARSHLY decreasing the minimum bandwidth required to not end up in a timeout

--- XSFileTransfer/Constants.cs
+++ XSFileTransfer/Constants.cs
@@ -4,9 +4,9 @@ namespace XSFileTransfer
 {
     public class Constants
     {
-        public const int MaxPacketSize = 32 * 1024 * 1024;     // 512MByte seems to be the absolute maximum for this
+        public const int MaxPacketSize = 1 * 1024 * 1024;   // the chunk size needs to go through before the timeout is hit
         public const int DefaultPort = 3648;
-        public const int DefaultTimeout = 10000;
+        public const int DefaultTimeout = 30000;            // 1 MByte per 30 seconds needs a minimum transfer rate of 32KByte/s
         public static int MaxConnections = 2;
 
         public static string DefaultReceiveFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\XSFileTransfer";
